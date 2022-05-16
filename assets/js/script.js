75c06189a3bd0121a8973446b4b22c6654a1459d
RhiH <rhi.herbert@gmail.com>
2022-05-16 17:17:45 +0000

    add getData function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,36 @@
 const section = document.querySelector('section');
+
+const getData = () => [
+    {imgSrc:"../assets/images/Beryllium.jpg", name:"beryllium"},
+    {imgSrc:"../assets/images/Boron.jpg", name:"boron"},
+    {imgSrc:"../assets/images/Carbon.jpg", name:"carbon"},
+    {imgSrc:"../assets/images/Fluorine.jpg", name:"fluorine"},
+    {imgSrc:"../assets/images/helium.jpg", name:"helium"},
+    {imgSrc:"../assets/images/hydrogen.jpg", name:"hydrogen"},
+    {imgSrc:"../assets/images/Lithium.jpg", name:"lithium"},
+    {imgSrc:"../assets/images/Neon.jpg", name:"neon"},
+    {imgSrc:"../assets/images/Nitrogen.jpg", name:"nitrogen"},
+    {imgSrc:"../assets/images/Oxygen.jpg", name:"oxygen"},
+    {imgSrc:"../assets/images/Beryllium.jpg", name:"beryllium"},
+    {imgSrc:"../assets/images/Boron.jpg", name:"boron"},
+    {imgSrc:"../assets/images/Carbon.jpg", name:"carbon"},
+    {imgSrc:"../assets/images/Fluorine.jpg", name:"fluorine"},
+    {imgSrc:"../assets/images/helium.jpg", name:"helium"},
+    {imgSrc:"../assets/images/hydrogen.jpg", name:"hydrogen"},
+    {imgSrc:"../assets/images/Lithium.jpg", name:"lithium"},
+    {imgSrc:"../assets/images/Neon.jpg", name:"neon"},
+    {imgSrc:"../assets/images/Nitrogen.jpg", name:"nitrogen"},
+    {imgSrc:"../assets/images/Oxygen.jpg", name:"oxygen"},
+    {imgSrc:"../assets/images/Beryllium.jpg", name:"beryllium"},
+    {imgSrc:"../assets/images/Boron.jpg", name:"boron"},
+    {imgSrc:"../assets/images/Carbon.jpg", name:"carbon"},
+    {imgSrc:"../assets/images/Fluorine.jpg", name:"fluorine"},
+    {imgSrc:"../assets/images/helium.jpg", name:"helium"},
+    {imgSrc:"../assets/images/hydrogen.jpg", name:"hydrogen"},
+    {imgSrc:"../assets/images/Lithium.jpg", name:"lithium"},
+    {imgSrc:"../assets/images/Neon.jpg", name:"neon"},
+    {imgSrc:"../assets/images/Nitrogen.jpg", name:"nitrogen"},
+    {imgSrc:"../assets/images/Oxygen.jpg", name:"oxygen"},
+]
+
+console.log(getData);
